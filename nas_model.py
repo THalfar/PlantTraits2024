a131289a5316fe81f4ebf62b9c3b5b2dfdb9e64d
Tobias Halfar <tobias.j.halfar@gmail.com>
2024-04-16 15:35:25 +0300

    Fixed the STUPIDY in making traintest df. Did forget image preprocessing before EffiecientNetV2 :(

--- nas_model.py
+++ nas_model.py
@@ -84,6 +84,7 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
     try:
         for gpu in gpus:
+            print(f'Setting memory growth for {gpu}')
             tf.config.experimental.set_memory_growth(gpu, True)
     except RuntimeError as e:
         print(e)
